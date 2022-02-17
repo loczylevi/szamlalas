5d00ac2a6f0f0b876677b4aa4dbf2fef666958a8
loczylevi <71691777+loczylevi@users.noreply.github.com>
2022-02-17 20:22:37 +0000

    generáltunk 5 db, veletlen szamot

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 """1. Feladat
 Készíts egy programot, amely [1;10] intervallumon generál 5 darab véletlen egész számot és ezeket tárolja el egy listában! A program számolja össze, és képernyőre írja ki a listában tárolt páros számok számát, valamint a lista elemeit!"""
 
+import random
+lista = []
+for szam in range(5):
+  veletlen = random.randint(1,10)
+  lista.append(veletlen)
+
+print(lista)
+  
