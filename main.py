364aae7f0ad02fc29eb7b6ecba7793f638dccd72
loczylevi <71691777+loczylevi@users.noreply.github.com>
2022-02-17 20:19:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+"""1. Feladat
+Készíts egy programot, amely [1;10] intervallumon generál 5 darab véletlen egész számot és ezeket tárolja el egy listában! A program számolja össze, és képernyőre írja ki a listában tárolt páros számok számát, valamint a lista elemeit!"""
+
